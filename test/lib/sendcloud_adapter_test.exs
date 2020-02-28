223e107e7ddc1bb9546d04494329388b9857d13d
blindingdark <blindingdark@outlook.com>
2020-02-29 01:47:30 +0800

    format.

--- test/lib/sendcloud_adapter_test.exs
+++ test/lib/sendcloud_adapter_test.exs
@@ -1,24 +1,29 @@
 defmodule Bamboo.SendcloudAdapterTest do
-  use ExUnit.Case,async: false
+  use ExUnit.Case, async: false
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   alias Bamboo.Email
   alias Bamboo.SendcloudAdapter
   alias Bamboo.SendcloudHelper
 
-  @config %{adapter: SendcloudAdapter, api_user: "sumi_developer_test_luTTRI", api_key: "ByRYrryfEVJmYxN2"}
+  @config %{
+    adapter: SendcloudAdapter,
+    api_user: "sumi_developer_test_luTTRI",
+    api_key: "ByRYrryfEVJmYxN2"
+  }
 
   setup_all do
     :ok
   end
 
   test "deliver/2 sends from, html and text body, subject, and headers" do
-    email = new_email(
-      subject: "My Subject",
-      text_body: "TEXT BODY",
-      html_body: "HTML BODY",
-    )
-    |> Email.put_header("Reply-To", "reply@example.com")
+    email =
+      new_email(
+        subject: "My Subject",
+        text_body: "TEXT BODY",
+        html_body: "HTML BODY"
+      )
+      |> Email.put_header("Reply-To", "reply@example.com")
 
     use_cassette "success_sample" do
       result =
@@ -32,7 +37,10 @@ defmodule Bamboo.SendcloudAdapterTest do
   test "deliver/2 sends template email" do
     email =
       new_email(subject: "Active")
-      |> SendcloudHelper.template("test_template_active", %{"%name%" => ["example_name"], "%content%" => ["example_content"]})
+      |> SendcloudHelper.template("test_template_active", %{
+        "%name%" => ["example_name"],
+        "%content%" => ["example_content"]
+      })
 
     use_cassette "template_success_sample" do
       result =
@@ -44,11 +52,15 @@ defmodule Bamboo.SendcloudAdapterTest do
   end
 
   defp new_email(attrs) do
-    attrs = Keyword.merge([
-      from: {"Test From", "test@test.sendcloud.org"},
-      to: [{"Jun", "jun@example.com"}]
-    ], attrs)
+    attrs =
+      Keyword.merge(
+        [
+          from: {"Test From", "test@test.sendcloud.org"},
+          to: [{"Jun", "jun@example.com"}]
+        ],
+        attrs
+      )
 
-    Email.new_email(attrs) |> Bamboo.Mailer.normalize_addresses
+    Email.new_email(attrs) |> Bamboo.Mailer.normalize_addresses()
   end
 end
